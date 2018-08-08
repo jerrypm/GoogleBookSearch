fad660432f2fb4f89b5b0bcebd652b244711b34e
Jeri Purnama Maulid <jeripm@jeris-macbook-pro.local>
2018-08-08 22:06:25 +0700

    Initial Commit

---
+++ DaftarBuku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DaftarBuku
+//
+//  Created by Jeri Purnama Maulid on 08/08/18.
+//  Copyright © 2018 Jeri Purnama Maulid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
